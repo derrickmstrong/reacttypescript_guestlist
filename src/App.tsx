fce3f02d7ba58a5b7778776fc854bcfd69947abc
Derrick Strong <derrickmstrong@gmail.com>
2021-09-29 09:29:19 -0500

    Declare type as number OR string

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState<number | string>(5)
+  const changeNumber = () => {
+    setNumber("10")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {number}
+      <button onClick={changeNumber}>Change to 10</button>
     </div>
   );
 }
