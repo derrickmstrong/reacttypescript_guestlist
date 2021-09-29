db8e2d6a8503ba050d9536e40cb806942144f9bf
Derrick Strong <derrickmstrong@gmail.com>
2021-09-29 10:38:13 -0500

    Render initial people state

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,17 @@ interface IState {
 }
 
 function App() {
+  
+  const initialPeople = [
+    {
+      name: 'Lebron James',
+      age: 36,
+      url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.pVPAT3G0Hzn_F242iFzK0wHaE7%26pid%3DApi&f=1',
+      note: 'The G.O.A.T.',
+    },
+  ];
 
-  const [people, setPeople] = useState<IState["people"]>([])
+  const [people, setPeople] = useState<IState["people"]>(initialPeople)
 
   return (
     <div className="App">
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -10,10 +10,24 @@ interface IProps {
 }
 
 const List: React.FC<IProps> = ({ people }) => {
+
+    const renderList: JSX.Element[] = people.map(person => {
+      return (
+        <li className='List'>
+            <div className="List-header">
+                <img src={person.url} alt="" className="List-img" />
+                <h2>{person.name}</h2>
+            </div>
+          <p>{person.age} years old</p>
+          <p className="List-note">{person.note}</p>
+        </li>
+      );
+    });
+
     return (
-        <div>
-            I am a list
-        </div>
+        <ul>
+            {renderList}
+        </ul>
     )
 }
 
